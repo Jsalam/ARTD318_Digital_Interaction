ec387b287ddd5e26736810b250fd416f91d2ef05
Juan Salamanca <jsal@wirelessprv-10-195-153-166.near.illinois.edu>
2018-02-28 13:46:34 -0600

    functions g2

--- functions/group2/sketch.js
+++ functions/group2/sketch.js
@@ -7,10 +7,22 @@ function setup(){
 
 function draw(){
 	background(240);
-	face(100,100,mouseX);
-	face(mouseX,300,mouseX);
+	//face(100,100,mouseX);
+	//face(mouseX,300,mouseX);
+crowd();
 }
 
+
+function crowd(){
+
+	var counter = 0;
+	while (counter < 20){
+		//face(random(width), random(height), 30);
+		face(50, 0 + counter*30, 30);
+		counter = counter+1;
+	}
+
+}
 
 
 function face(orgX, orgY, diam){
