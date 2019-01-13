617600df1aac186d400c7f8ca08a5aae101f24d1
Juan Salamanca <juans123456@gmail.com>
2019-01-12 18:55:14 -0600

    Function added to 2019 folder

---
+++ 2019/functions/src/sketch.js
@@ -0,0 +1,53 @@
+
+
+function setup(){
+	createCanvas(400,400);
+
+}
+
+function draw(){
+	// Set the background
+	background(240);
+	// Draw the menu
+	menu();
+}
+
+function menu(){
+
+	drawRectangle(40,10);
+	drawRectangle(40,100);
+	drawRectangle(40,190);
+	drawRectangle(40,280);
+
+}
+
+
+function drawRectangle(orgX, orgY){
+	w = 300;
+	h = 80;
+
+	// Evaluate mouse Over
+	var overX = mouseX > orgX && mouseX < orgX + w;
+	var overY = mouseY > orgY && mouseY < orgY + h;
+
+	// Determine if the mouse is over the rectangle
+	if ( overX && overY ){
+		// Over color
+		if (mouseIsPressed){
+			// Set to orange
+			fill(250,150,0,100);
+			// Change width
+			w = 150;
+			// control X position with mouse
+			orgX = mouseX;
+		} else {
+			// Set to red
+			fill(250,0,0,100);
+		}
+	} else {
+		// No over color
+		fill(250,0,0,10);
+	}
+	// Draw rectangle
+	rect (orgX, orgY,w,h);
+}
